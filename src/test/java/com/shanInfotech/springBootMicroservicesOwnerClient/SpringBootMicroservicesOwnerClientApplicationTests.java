350fff1fee5aa01d18961ece98baa9e437fc6cf8
ShanInfotechSolutions <shanmukh@shaninfotechsolutions.com>
2025-09-03 15:42:40 +0530

    test cases added

--- src/test/java/com/shanInfotech/springBootMicroservicesOwnerClient/SpringBootMicroservicesOwnerClientApplicationTests.java
+++ src/test/java/com/shanInfotech/springBootMicroservicesOwnerClient/SpringBootMicroservicesOwnerClientApplicationTests.java
@@ -2,12 +2,20 @@ package com.shanInfotech.springBootMicroservicesOwnerClient;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+  // minimal config for JwtService
+  "jwt.secret=AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=",
+  "jwt.expiration-minutes=60",
+  // keep cloud bits off during tests
+  "eureka.client.enabled=false",
+  "spring.cloud.discovery.enabled=false"
+})
 class SpringBootMicroservicesOwnerClientApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {}
 }
+

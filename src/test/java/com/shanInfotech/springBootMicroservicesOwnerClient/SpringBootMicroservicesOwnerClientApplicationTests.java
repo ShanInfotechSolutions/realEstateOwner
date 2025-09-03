5b6d16b675af4b8b9f115e11706f88a18ee83f81
ShanInfotechSolutions <shanmukh@shaninfotechsolutions.com>
2025-09-03 17:29:10 +0530

    test configures

--- src/test/java/com/shanInfotech/springBootMicroservicesOwnerClient/SpringBootMicroservicesOwnerClientApplicationTests.java
+++ src/test/java/com/shanInfotech/springBootMicroservicesOwnerClient/SpringBootMicroservicesOwnerClientApplicationTests.java
@@ -1,23 +1,37 @@
 package com.shanInfotech.springBootMicroservicesOwnerClient;
+
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.test.context.TestPropertySource;
+
+import com.shanInfotech.springBootMicroservicesOwnerClient.repository.OwnerRepository;
 
 @SpringBootTest
 @TestPropertySource(properties = {
-  // JWT for JwtService
+  // JWT needed by JwtService
   "jwt.secret=AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=",
   "jwt.expiration-minutes=60",
-  // turn off discovery/cloud
+
+  // keep service discovery off
   "eureka.client.enabled=false",
   "spring.cloud.discovery.enabled=false",
-  "spring.autoconfigure.exclude=" +
-    "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration," +
-    "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration," +
-    "org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration," +
-    "org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration"
+
+  // Keep DB/JPA OFF for this smoke test
+  "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration,org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration,org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration"
 })
 class SpringBootMicroservicesOwnerClientApplicationTests {
+
+  // Provide a mock OwnerRepository so OwnerService can be created
+  @TestConfiguration
+  static class Mocks {
+    @Bean
+    OwnerRepository ownerRepository() {
+      return Mockito.mock(OwnerRepository.class);
+    }
+  }
 
   @Test
   void contextLoads() {}

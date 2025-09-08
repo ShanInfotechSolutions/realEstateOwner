55b43c44ab21fb433b8fdd7c8bc8961fe9a85e40
ShanInfotechSolutions <shanmukh@shaninfotechsolutions.com>
2025-09-08 12:18:14 +0530

    modified main class

--- src/main/java/com/shanInfotech/springBootMicroservicesOwnerClient/SpringBootMicroservicesOwnerClientApplication.java
+++ src/main/java/com/shanInfotech/springBootMicroservicesOwnerClient/SpringBootMicroservicesOwnerClientApplication.java
@@ -3,19 +3,12 @@ package com.shanInfotech.springBootMicroservicesOwnerClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-@RestController
+
 @EnableDiscoveryClient
 @SpringBootApplication
 public class SpringBootMicroservicesOwnerClientApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootMicroservicesOwnerClientApplication.class, args);
-	}
-
-	@GetMapping("/getOwner")
-	public String getOwner() {
-		return "Hello Owner";
 	}
 }
